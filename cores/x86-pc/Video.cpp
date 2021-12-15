33468f394af83907560ac7a4821c94636a063fd0
Jean THOMAS <virgule@jeanthomas.me>
2021-12-15 23:57:01 +0100

    Video.h API rework
     * Removes putchar, uses print() method overloading instead
     * Adds methods for interacting with video mode and cursor position

--- cores/x86-pc/Video.cpp
+++ cores/x86-pc/Video.cpp
@@ -4,13 +4,24 @@ extern "C" {
 
 #include "Video.h"
 
-void Video::putchar(char c) const {
+void Video::print(char c) const {
 	videoasm_putchar(c);
 }
 
 void Video::print(const char *str) const {
-	const char *ptr;
-	for (ptr = str; *ptr; ptr++) {
-		putchar(*ptr);
+	for (const char *ptr = str; *ptr; ptr++) {
+		videoasm_putchar(*ptr);
 	}
+}
+
+void Video::clear() const {
+	videoasm_clear();
+}
+
+void Video::setCursorPosition(char x, char y) const {
+	videoasm_set_cursor_position(x, y);
+}
+
+void Video::setMode(VideoMode mode) const {
+	videoasm_set_video_mode(mode);
 }
--- cores/x86-pc/Video.h
+++ cores/x86-pc/Video.h
@@ -1,9 +1,20 @@
 #pragma once
 
+enum VideoMode {
+	TEXT_40X25_BW = 0x00,
+	TEXT_40X25_COLOR = 0x01,
+	TEXT_80X25_BW = 0x02,
+	TEXT_80X25_COLOR = 0x03,
+	GRAPHIC_320X200_COLOR = 0x04,
+	GRAPHIC_320X200_BW = 0x05,
+	GRAPHIC_640X200_COLOR = 0x06,
+};
+
 class Video {
 public:
-	Video();
-
-	void putchar(char c) const;
 	void print(const char *str) const;
+	void print(char c) const;
+	void clear() const;
+	void setCursorPosition(char x, char y) const;
+	void setMode(VideoMode mode) const;
 };
