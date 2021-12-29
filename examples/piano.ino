476b03e884775b9e8a8d64db1ce0ed95a7afb712
Jean THOMAS <virgule@jeanthomas.me>
2021-12-29 22:26:46 +0100

    Examples: update piano.ino

--- examples/piano.ino
+++ examples/piano.ino
@@ -5,31 +5,41 @@ Keyboard key;
 
 void setup() {
   vid.clear();
-  vid.setCursorPosition(0,0);
-  vid.print("Piano time!");
+  vid.setCursorPosition(0, 0);
+  vid.println("\r\nPiano time!\r\n");
+  vid.println("_________________________________________");
+  vid.println("|  |#| |#|  |  |#| |#| |#|  |  |#| |#|  |");
+  vid.println("|  |#| |#|  |  |#| |#| |#|  |  |#| |#|  |");
+  vid.println("|  |#| |#|  |  |#| |#| |#|  |  |#| |#|  |");
+  vid.println("|  |#| |#|  |  |#| |#| |#|  |  |#| |#|  |");
+  vid.println("|   |   |   |   |   |   |   |   |   |   |");
+  vid.println("| A | S | D | F | G | H | J | K | L | ; |");
+  vid.println("|___|___|___|___|___|___|___|___|___|___|");
 }
 
-char prevKey = 0;
 void loop() {
   if (key.available()) {
-    char curKey = key.getch();
-    if (curKey != prevKey) {
-      switch (curKey) {
-        case 'a': tone(1193180/261); break; // C4
-          case 'w': tone(1193180/277); break; // C4#
-        case 's': tone(1193180/293); break; // D4
-          case 'e': tone(1193180/311); break; // D4#
-        case 'd': tone(1193180/329); break; // E4
-        case 'f': tone(1193180/349); break; // F4
-          case 't': tone(1193180/369); break; // F4#
-        case 'g': tone(1193180/392); break; // G4
-          case 'y': tone(1193180/415); break; // G4#
-        case 'h': tone(1193180/440); break; // A4
-          case 'u': tone(1193180/466); break; // A4#
-        case 'j': tone(1193180/493); break; // B4
-        case 'k': tone(1193180/523); break; // C5
-        default: noTone(); break;
-      }
+    switch (key.getch()) {
+      case 'a': tone(261); break; // C4
+      case 'w': tone(277); break; // C4#
+      case 's': tone(293); break; // D4
+      case 'e': tone(311); break; // D4#
+      case 'd': tone(329); break; // E4
+      case 'f': tone(349); break; // F4
+      case 't': tone(369); break; // F4#
+      case 'g': tone(392); break; // G4
+      case 'y': tone(415); break; // G4#
+      case 'h': tone(440); break; // A4
+      case 'u': tone(466); break; // A4#
+      case 'j': tone(493); break; // B4
+      case 'k': tone(523); break; // C5
+      case 'o': tone(554); break; // C5#
+      case 'l': tone(587); break; // D5
+      case 'p': tone(622); break; // D5#
+      case ';': tone(659); break; // E5
+      default: return; break;
     }
+    delayMicroseconds(50000);
+    noTone();
   }
 }
