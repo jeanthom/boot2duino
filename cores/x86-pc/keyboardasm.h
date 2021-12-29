1c6b68352c6462f3567c9b38de156a70c185a592
Jean THOMAS <virgule@jeanthomas.me>
2021-12-29 22:26:18 +0100

    Keyboard: code styling, and minor improvements

--- cores/x86-pc/keyboardasm.h
+++ cores/x86-pc/keyboardasm.h
@@ -1,3 +1,5 @@
+#pragma once
+
 extern __attribute__ ((fastcall)) char keyboardasm_available(void);
 extern __attribute__ ((fastcall)) char keyboardasm_getch(void);
 extern __attribute__ ((fastcall)) unsigned char keyboardasm_getkeycode(void);
