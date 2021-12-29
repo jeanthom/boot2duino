d740920e110a5d0bb1629a6b833757370b71ee6c
Jean THOMAS <virgule@jeanthomas.me>
2021-12-29 22:25:29 +0100

    Audio: improve support

--- cores/x86-pc/Tone.cpp
+++ cores/x86-pc/Tone.cpp
@@ -1,10 +1,15 @@
 extern "C" {
 	#include "toneasm.h"
+	#include "delay.h"
 }
 #include "Tone.h"
 
 void tone(unsigned int freq, unsigned long duration) {
-	toneasm_toneon(freq, 0, 0);
+	toneasm_toneon(1193180/freq);
+	if (duration) {
+		delay(duration);
+		noTone();
+	}
 }
 
 void noTone() {
